9918e30717d2357892713d7aab1f334a35600f2b
Kai <kaiquan.s@outlook.com>
2021-10-10 15:35:33 +1100

    Handling Errors

--- src/components/Products/AvailableProducts.js
+++ src/components/Products/AvailableProducts.js
@@ -6,12 +6,17 @@ import { useEffect, useState } from "react";
 const AvailableProducts = () => {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(null);
 
   useEffect(() => {
     const fetchProducts = async () => {
       const response = await fetch(
         "https://orderexpress-fda47-default-rtdb.asia-southeast1.firebasedatabase.app/products.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
       const responseData = await response.json();
       const loadProducts = [];
       for (const key in responseData) {
@@ -25,13 +30,25 @@ const AvailableProducts = () => {
       setProducts(loadProducts);
       setIsLoading(false);
     };
-    fetchProducts();
+
+    fetchProducts().catch((error) => {
+      setIsLoading(false);
+      setIsError(error.message);
+    });
   }, []);
 
   if (isLoading) {
     return (
       <section className={classes.prodLoading}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (isError) {
+    return (
+      <section className={classes.prodError}>
+        <p>{isError}</p>
       </section>
     );
   }
